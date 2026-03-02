72058db65b3b7328f60c8f7b5bd9b7c987eb4da8
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-02 20:22:40 +0500

    fix: next.js config remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com"], 
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
