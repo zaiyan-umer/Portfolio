db284ce71a1a72f5ec67db71a6dbb857cb7d3f09
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-02 20:19:43 +0500

    feat: add certifications

--- components/data/course-work.ts
+++ components/data/course-work.ts
@@ -1,5 +1,14 @@
 export const certifications = [
     {
+        title: "Next.js Fundamentals, v4",
+        issuedBy: "Frontend Masters",
+        img: "/frontend-masters.webp",
+        issuedAt: "Feb 2026",
+        credentialId: "778d1beae6/bCWFkdHrBA",
+        link: "https://static.frontendmasters.com/ud/c/778d1beae6/bCWFkdHrBA/next-js-v4.pdf",
+        tags: ["Next.js", "Typescript", "Edge Computing"]
+    },
+    {
         title: "META Front-End Developer",
         issuedBy: "META",
         img: "/meta.webp",
@@ -9,13 +18,13 @@ export const certifications = [
         tags: ["HTML", "CSS", "Javascript", "React.js", "UI/UX"]
     },
     {
-        title: "Next.js Fundamentals, v4",
-        issuedBy: "Frontend Masters",
-        img: "/frontend-masters.webp",
-        issuedAt: "Feb 2026",
-        credentialId: "778d1beae6/bCWFkdHrBA",
-        link: "https://static.frontendmasters.com/ud/c/778d1beae6/bCWFkdHrBA/next-js-v4.pdf",
-        tags: ["Next.js", "Typescript", "Edge Computing"]
+        title: "META React Specialization",
+        issuedBy: "META",
+        img: "/meta.webp",
+        issuedAt: "Jul 2025",
+        credentialId: "72WKMSBO3QRO",
+        link: "https://www.coursera.org/account/accomplishments/professional-cert/certificate/72WKMSBO3QRO",
+        tags: ["React.js", "Custom Hooks", "Javascript"]
     }
 ]
 
