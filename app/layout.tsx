ae869966789304e8de8ca2513a548d594a355cae
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-20 14:42:25 +0500

    chore: fix meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "Zaiyan Umer · Portfolio",
+    default: "Zaiyan Umer | Portfolio",
     template: "%s · Zaiyan Umer",
   },
   description:
